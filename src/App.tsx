3f23ab1fcc0b41e61b9d76d86f480990ee36f676
Andry <andrysitrakiniainasoa@gmail.com>
2026-01-08 23:47:34 +0300

    add some table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import "./App.css";
+
+
+const responses = [
+  {
+    id: 0,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem recusandae dolores provident consectetur, eius accusantium quaerat reprehenderit voluptatum nemo amet tempore. Nobis possimus facilis quisquam omnis quo a, inventore culpa laboriosam maiores nisi dolorem totam aperiam eveniet quaerat earum ducimus. Tempore nulla laudantium sequi nam deserunt quis eveniet, quod, perspiciatis quaerat non ullam maiores atque deleniti quas dolor. Modi excepturi impedit placeat, optio natus, aliquid neque, corrupti dolores possimus sed facilis qui commodi exercitationem corporis ad itaque quo molestiae odit. Eveniet repellat optio quisquam aperiam officia enim hic iste dolores itaque eaque in totam at rem sit, minima quidem. Voluptatum.",
+    prediction: 0.3,
+  },
+  {
+    id: 1,
+    text: "this is one text",
+    prediction: 0.3,
+  },
+  {
+    id: 2,
+    text: "this is one text",
+    prediction: 0.3,
+  },
+  {
+    id: 3,
+    text: "this is one text",
+    prediction: 0.3,
+  },
+  {
+    id: 4,
+    text: "this is one text",
+    prediction: 0.3,
+  },
+  {
+    id: 5,
+    text: "this is one text",
+    prediction: 0.3,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const renderPredictionIcon = (prediction: number) => {
+    return <span>{prediction}</span>;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="max-w-4xl mx-auto">
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance mt-6 mb-9">Sentiment analysis</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-lg font-bold">Text</TableHead>
+            <TableHead className="text-lg font-bold w-[10px]">Prediction</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {responses.map((response) => (
+            <TableRow key={response.id}>
+              <TableCell className="text-left whitespace-normal pe-3">{response.text}</TableCell>
+              <TableCell>{renderPredictionIcon(response.prediction)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </main>
+  );
 }
 
-export default App
+export default App;
